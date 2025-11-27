91952d9264cd06b266c6ebd9eb7023f97a2d7f39
HardikTi13 <tiwarihardik131105@gmail.com>
2025-11-27 16:57:33 +0530

    Create WaterTracker contract with addWater function
    Implement WaterTracker contract to track water consumption.

--- contracts/watertracker.sol
+++ contracts/watertracker.sol
@@ -1,1 +1,19 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract WaterTracker {
+
+    uint public totalWater;  // This will store total water consumed in ml
+
+    // Constructor runs only once when the contract is deployed
+    constructor() {
+        totalWater = 0;  // Start with 0 ml of waterFile explorer
+
+    }
+
+    // This function lets you add water (example: 250, 500)
+    function addWater(uint amount) public {
+        totalWater = totalWater + amount;
+    }
+}
